1789ad89f43c619683268e249bb4f9836e138dbf
Minin <70429604+mininxd@users.noreply.github.com>
2026-04-28 22:06:59 +0700

    Update tryToast.js

--- docs/js/tryToast.js
+++ docs/js/tryToast.js
@@ -105,7 +105,7 @@ dload.addEventListener('click', function() {
 })
 
 docs.addEventListener('click', function() {
-  window.location.assign("https://docs.mininxd.web.id/xdtoast.html")
+  window.location.assign("https://mininxd.vercel.app/")
   })
 
 
